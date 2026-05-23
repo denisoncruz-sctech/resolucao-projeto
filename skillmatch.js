00845cc00f8d450e21cf2508e8c3da9cc2a79d65
josianeda-franca-sctec <josiane_da-franca@estudante.sesisenai.org.br>
2026-05-22 21:56:31 -0300

    feat: Adiconando dados do candidato e vagas

---
+++ skillmatch.js
@@ -0,0 +1,44 @@
+//1. Dados principais do candidato
+
+const candidato = {
+  nome: "Josiane",
+  area: "Front-End",
+  habilidades: [
+    "JavaScript",
+    "GitHub",
+    "Lógica de Programação",
+    "Kanban"
+  ],
+  experienciaMeses: 3
+};
+
+//2. Lista de vagas disponíveis
+const vagas = [
+  {
+    id: 1,
+    empresa: "Itau Unibank",
+    cargo: "Desenvolvedor Front-End Junior",
+    requisitos: ["JavaScript", "GitHub", "Lógica de Programação"],
+    salario: 5000,
+    modalidade: "Remoto"
+  },
+  {
+    id: 2,
+    empresa: "Softplan",
+    cargo: "Estágio Front-End",
+    requisitos: ["JavaScript", "Kanban", "GitHub"],
+    salario: 3000,
+    modalidade: "Híbrido"
+  },
+  {
+    id: 3,
+    empresa: "Hospital Unimed Litoral",
+    cargo: "Programador JavaScript Junior",
+    requisitos: ["JavaScript", "Arrays", "Objetos", "Funções"],
+    salario: 3000,
+    modalidade: "Presencial"
+  }
+];
+
+console.log(candidato);
+console.log(vagas);
